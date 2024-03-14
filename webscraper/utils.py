0bbb503db4cb3e08eca183e71b643ddec9c19779
Kacper-Mila <kacper.mila@gmail.com>
2024-03-14 19:54:05 +0100

    Update web scraper templates and utils.py

--- webscraper/utils.py
+++ webscraper/utils.py
@@ -3,15 +3,12 @@ import requests
 
 
 def get_random_productid():
-    print("Getting random productid")
-
-    random_productid = "".join(random.choices("1234567890", k=8))
+    random_productid = "".join(random.choices("1234567890", k=random.randint(8, 9)))
 
     try:
         response = requests.get(
             f"https://www.ceneo.pl/{random_productid}", allow_redirects=False
         )
-        print(response.status_code)
         response.raise_for_status()
         if response.status_code == 301:
             raise requests.exceptions.RequestException
